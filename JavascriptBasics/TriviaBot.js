3b41c83eb3a2da5928c98a2390924e72ac3646d6
Sumit Mukharjee <sumitxxmukharjee@gmail.com>
2025-02-12 13:28:47 +0530

    another small projects

---
+++ JavascriptBasics/TriviaBot.js
@@ -0,0 +1,37 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName, botLocation, favoriteLanguage;
+botName = "Bixby";
+botLocation = "Korea";
+favoriteLanguage = "Samsung";
+
+console.log("My name is " + botName + " and I live in " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  favoriteLanguage +
+  " was originally called Mocha and then LiveScript before finally being renamed JavaScript—even though it has no real connection to Java! ☕😂";
+console.log(codingFact);
+
+codingFact =
+  "In 1995, Brendan Eich developed " +
+  favoriteLanguage +
+  " while working at Netscape. The company wanted a lightweight scripting language for web browsers, and Eich had only 10 days to create it. Despite its rushed development, " +
+  favoriteLanguage +
+  " became one of the most popular programming languages in the world! 🌎🔥";
+console.log(codingFact);
+
+codingFact =
+  favoriteLanguage +
+  " can run outside the browser! Most people associate JavaScript with web development, but thanks to Node.js, " +
+  favoriteLanguage +
+  " can be used for backend development, scripting, game development, IoT, and even AI!";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    "."
+);
